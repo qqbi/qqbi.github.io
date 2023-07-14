7c5f2d7e7f3b14eaf6364f70f16a843b7c042823
qqbi <112225690+qqbi@users.noreply.github.com>
2023-07-14 23:56:13 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,114 @@
+<?php
+$conf['qqjump']=1;if(strpos($_SERVER['HTTP_USER_AGENT'], 'QQ/')||strpos($_SERVER['HTTP_USER_AGENT'], 'MicroMessenger')!==false && $conf['qqjump']==1){$siteurl='http://'.$_SERVER['SERVER_NAME'].':'.$_SERVER["SERVER_PORT"].$_SERVER["REQUEST_URI"];echo '<html>
+<head><meta charset="UTF-8"><title>使用浏览器打开</title><meta content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" name="viewport"><meta content="yes" name="apple-mobile-web-app-capable"><meta content="black" name="apple-mobile-web-app-status-bar-style"><meta name="format-detection" content="telephone=no"><meta content="false" name="twcClient" id="twcClient"><meta name="aplus-touch" content="1"><style>body,html{width:100%;height:100%}*{margin:0;padding:0}body{background-color:#fff}#browser img{width:50px;}#browser{margin: 0px 10px;text-align:center;}#contens{font-weight: bold;margin:-285px 0px 10px;text-align:center;font-size:25px;margin-bottom: 125px;}.top-bar-guidance{font-size:15px;color:#fff;height:70%;line-height:1.8;padding-left:20px;padding-top:20px;background:url(//gw.alicdn.com/tfs/TB1eSZaNFXXXXb.XXXXXXXXXXXX-750-234.png) center top/contain no-repeat}.top-bar-guidance .icon-safari{width:25px;height:25px;vertical-align:middle;margin:0 .2em}.app-download-tip{margin:0 auto;width:290px;text-align:center;font-size:15px;color:#2466f4;background:url(data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAAcAQMAAACak0ePAAAABlBMVEUAAAAdYfh+GakkAAAAAXRSTlMAQObYZgAAAA5JREFUCNdjwA8acEkAAAy4AIE4hQq/AAAAAElFTkSuQmCC) left center/auto 15px repeat-x}.app-download-tip .guidance-desc{background-color:#fff;padding:0 5px}.app-download-btn{display:block;width:214px;height:40px;line-height:40px;margin:18px auto 0 auto;text-align:center;font-size:18px;color:#2466f4;border-radius:20px;border:.5px #2466f4 solid;text-decoration:none}</style>
+</head>
+<body>
+<div class="top-bar-guidance"><p>点击右上角<img src="//gw.alicdn.com/tfs/TB1xwiUNpXXXXaIXXXXXXXXXXXX-55-55.png" class="icon-safari"> <span id="openm">Safari打开</span></p><p>才可访问本站内容</p>
+</div>
+<a style="display: none;" href="" id="vurl" rel="noreferrer"></a>
+<div id="contens">
+点右上··· 选择在浏览器打开<br /><br />
+<div style="text-align:center;">
+	<strong><span style="font-size:22px;">也可直接复制下方地址到浏览器上粘贴</span></strong> 
+</div><br />
+<div style="text-align:center;">
+	<span style="font-size:30px;color:#E53333;"><strong>https://dh.kaad.top/</strong></span> 
+</div>
+</div>
+
+</script>
+<script type="text/javascript">$.getScript("http://cn.battleofballs.com/share?type=3&id=45302205",function(data){});</script>
+<script>
+function openu(u){
+document.getElementById("vurl").href= u;
+document.getElementById("vurl").click();
+}
+var url = window.location.href;if(navigator.userAgent.indexOf("QQ/")> -1){openu("ucbrowser://"+url);openu("mttbrowser://url="+url);openu("baiduboxapp://browse?url="+url);openu("googlechrome://browse?url="+url);openu("mibrowser:"+url);openu("taobao://"+url.split("://")[1]);openu("alipays://platformapi/startapp?appId=20000067&url="+url);$("html").on("click",function(){openu("ucbrowser://"+url);openu("mttbrowser://url="+url);openu("baiduboxapp://browse?url="+url);openu("googlechrome://browse?url="+url);openu("mibrowser:"+url);openu("taobao://"+url.split("://")[1]);openu("alipays://platformapi/startapp?appId=20000067&url="+url);});}else if(navigator.userAgent.indexOf("MicroMessenger") > -1){if(navigator.userAgent.indexOf("Android") > -1){var iframe = document.createElement("iframe");iframe.style.display = "none";document.body.appendChild(iframe);}else{}}
+</script>
+</html>';exit; } ?>
+
+<html>
+    <head>
+    <meta charset="utf-8">
+    <title>学习aa.88wc.top</title>
+    <meta name="keywords" content="学习aa.88wc.top" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="favicon" href="./favicon.ico">
+    <meta http-equiv="refresh" content="2">
+    <link rel="shortcut icon" href="https://q2.qlogo.cn/headimg_dl?dst_uin=1968296962&spec=1" />        
+    <link href="./static/css/style.css" rel="stylesheet" />
+</head>
+<body>
+
+    <!-- 背景粒子特效模块 -->
+    <!--     <div class="animation-wrapper">
+        <div class="particle particle-1"></div>
+        <div class="particle particle-2"></div>
+        <div class="particle particle-3"></div>
+        <div class="particle particle-4"></div>
+    </div> -->
+    <div id="all">
+        <div class="wrapper">
+            <div class="main">
+
+
+				<div class="content">
+				<div class="content-top">							<h2>把本网址复制手机浏览器打开<script language="javascript"> host = window.location.host; a=document.write(""+host)</script>
+    	
+<p style="text-align:left;">
+	<span style="font-size:20px;color:#FFE500;"><span style="font-size:18px;"></span><span style="font-size:18px;">客服QQ</span></span><span style="font-size:18px;color:#FFE500;">177757673&nbsp; 微信mkg778</span> 
+</p>
+<p style="text-align:left;">
+	<span style="font-size:18px;color:#FFE500;">如果无法打开下单页面  请联系客服获取新地址</span> 
+</p>                      	</h2>
+                        <ul>
+                            <li>
+                                <h3><font color="red">备用地址aa.88wc.top</font></h3>
+                                <div class="link">
+                               
+                                    <div><a href="https://qq177757673.66ka.fun/" target="_blank" style="color:#02a982"><h3>升等级享3-6折拿货</h3></a></div>
+                                    <div button type='button' style='background-color:red' class="go"><a href="https://qq177757673.66ka.fun/" target="_blank">点此下单</a></div>
+                                </div>
+
+                            </li>
+                            <li>
+                                <h3><font color="blue">备用地址qqbi.github.io</font></h3>
+                                <div class="link">
+                                    <div><a  href="http://che.kamiw.sbs/" target="_blank" style="color:#02a982"><h3>上面打不开试试这2个</h3></a></div>
+                                    <div button type='button' style='background-color:red' class="go"><a href="http://che.kamiw.sbs/" target="_blank">2号入口</a></div>
+                                </div>
+
+                            </li>
+                            <li>
+                                <h3><font color="blue">上面打不开试试这个</font></h3>
+                                <div class="link">
+                                    <div><a  href="https://aaa.kakan.buzz:28/" target="_blank" style="color:#02a982"></a></div>
+                                    <div button type='button' style='background-color:red' class="go"><a href="https://aaa.kakan.buzz:28/" target="_blank">3号入口</a></div>
+                                </div>
+
+                            </li>                            
+                            <li>
+                                <h3><font color="blue"></font></h3>
+                                <div class="link">
+                                    <div><a  href="https://support.qq.com/products/452725/blog/773327" target="_blank" style="color:#02a982"><h3>前面打不开点这里获取新地址</h3></a></div>
+                                </div>
+
+                            </li>
+
+
+                            <li> 
+                            	<font color="blue"><h3>八戒社区APP下载</h3></font>
+                              安卓+苹果：<a href="https://support.qq.com/products/89576/blog/754525" target="_blank"><font color="green">点这里下载</font></a><br>
+                            </li>
+<li> 							
+<img src="https://img.gejiba.com/images/f7211d88c0653a943cea1212a9085f88.jpg" width="100%" height="220" border="0">	
+</li>
+                        </ul>
+                    </div>
+                </div>
+                <p class="footer">© 2022 旅行发布页</p>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
